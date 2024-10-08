775d30ea20b606460e112d136dd4ced452edcd87
João Vitor Alves Segrini <joaovsegrini@gmail.com>
2024-10-08 08:45:47 -0300

    SwitchCase

---
+++ ExpressCondTernaria/ExpressCondTernaria/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+/*
+var preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double desconto;
+if (preco < 20.0) {
+    desconto = preco * 0.1;
+} else {
+    d 
+*/
+double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+Console.WriteLine(desconto);
