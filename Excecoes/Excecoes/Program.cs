bc65e7728355e38cceb6015421bafecc75bf6b05
João Vitor Alves Segrini <joaovsegrini@gmail.com>
2024-10-29 09:23:14 -0300

    Excecoes

---
+++ Excecoes/Excecoes/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data
+de entrada e data de saída) e mostrar os dados da reserva, inclusive sua duração em
+dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
+novamente a reserva com os dados atualizados. O programa não deve aceitar dados
+inválidos para a reserva, conforme as seguintes regras:
+- Alterações de reserva só podem ocorrer para datas futuras
+- A data de saída deve ser maior que a data de entrada  */
+
+using Excecoes.Entities;
+using Excecoes.Entities.Exceptions;
+
+try
+{
+    Console.Write("Room number: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.Write("Check-in date (dd/MM/yyyy): ");
+    DateTime checkIn = DateTime.Parse(Console.ReadLine());
+    Console.Write("Check-out date (dd/MM/yyyy): ");
+    DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+
+    Reservation reservation = new Reservation(number, checkIn, checkOut);
+    Console.WriteLine("Reservation: " + reservation);
+
+    Console.WriteLine();
+    Console.WriteLine("Enter data to update the reservation:");
+    Console.Write("Check-in date (dd/MM/yyyy): ");
+    checkIn = DateTime.Parse(Console.ReadLine());
+    Console.Write("Check-out date (dd/MM/yyyy): ");
+    checkOut = DateTime.Parse(Console.ReadLine());
+
+    reservation.UpdateDates(checkIn, checkOut);
+    Console.WriteLine("Reservation: " + reservation);
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Error in reservation: " + e.Message);
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Format error: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Unexpected error: " + e.Message);
+}
+
+
