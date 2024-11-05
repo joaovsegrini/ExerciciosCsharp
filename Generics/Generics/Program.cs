1cdf4c2d63ab0c349dd4df387d554c108809a5cc
João Vitor Alves Segrini <joaovsegrini@gmail.com>
2024-11-05 08:54:34 -0300

    Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿/*  Deseja-se fazer um programa que leia um conjunto de N números inteiros (N de1 a 10), e depois 
+ *  imprima esses números de forma organizada conformeexemplo. Em seguida, informar qual foi o primeiro valor informado. */
+
+using Generics;
+
+PrintService<int> printService = new PrintService<int>();
+
+Console.Write("How many values? ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First: " + printService.First());
