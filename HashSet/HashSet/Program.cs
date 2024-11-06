18ea19d568364ef0544a8554004233dc97728ae5
João Vitor Alves Segrini <joaovsegrini@gmail.com>
2024-11-06 09:52:29 -0300

    Hash set AND SortedSet

---
+++ HashSet/HashSet/Program.cs
@@ -0,0 +1,14 @@
+﻿static void Main(string[] args)
+{
+    HashSet<string> set = new HashSet<string>();
+
+    set.Add("TV");
+    set.Add("Notebook");
+    set.Add("Tablet");
+
+    Console.WriteLine(set.Contains("Notebook"));
+    foreach (String p in set)
+    {
+        Console.WriteLine(p);
+    }
+}
